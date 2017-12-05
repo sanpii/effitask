344b6f298aba2586a8386a9c791211b72916471c
Sanpi <sanpi@homecomputing.fr>
2017-12-05 11:24:53 +0100

    Display finished attribut

--- src/widgets/tasks.rs
+++ src/widgets/tasks.rs
@@ -13,15 +13,44 @@ pub enum Msg {
     Update(Vec<::todo_txt::Task>),
 }
 
+#[repr(u32)]
+enum Column {
+    Finished = 0,
+    Subject = 1,
+}
+
+impl ::std::convert::Into<u32> for Column
+{
+    fn into(self) -> u32
+    {
+        unsafe {
+            ::std::mem::transmute(self)
+        }
+    }
+}
+
+impl ::std::convert::Into<i32> for Column
+{
+    fn into(self) -> i32
+    {
+        unsafe {
+            ::std::mem::transmute(self)
+        }
+    }
+}
+
 impl Tasks
 {
     pub fn populate(&mut self, tasks: Vec<::todo_txt::Task>)
     {
+        use ::gtk::ToValue;
+
         self.model.clear();
 
         for task in tasks.iter() {
             let row = self.model.append();
-            self.model.set_value(&row, 0, &gtk::Value::from(&task.subject));
+            self.model.set_value(&row, Column::Finished.into(), &task.finished.to_value());
+            self.model.set_value(&row, Column::Subject.into(), &task.subject.to_value());
         }
     }
 }
@@ -33,16 +62,21 @@ impl ::relm::Widget for Tasks
     {
         self.tree_view.set_model(Some(&self.model));
 
+        let column = gtk::TreeViewColumn::new();
+        self.tree_view.append_column(&column);
+
+        let cell = gtk::CellRendererToggle::new();
+        column.pack_start(&cell, false);
+        column.add_attribute(&cell, "active", Column::Finished.into());
+
         let cell = gtk::CellRendererText::new();
-        let view_column = gtk::TreeViewColumn::new();
-        view_column.pack_start(&cell, true);
-        view_column.add_attribute(&cell, "text", 0);
-        self.tree_view.append_column(&view_column);
+        column.pack_start(&cell, true);
+        column.add_attribute(&cell, "text", Column::Subject.into());
     }
 
     fn model(_: ()) -> gtk::ListStore
     {
-        let columns = vec![gtk::Type::String];
+        let columns = vec![gtk::Type::Bool, gtk::Type::String];
 
         gtk::ListStore::new(&columns)
     }

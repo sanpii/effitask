8deb0b575bd534bc30b04f25ac2374d2675571d6
Sanpi <sanpi@homecomputing.fr>
2020-04-05 08:22:01 +0200

    Fixes clippy warning

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
     dotenv::dotenv().ok();
 
     if std::env::args().nth(1) == Some("usage".to_string()) {
-        usage(&std::env::args().nth(0).unwrap());
+        usage(&std::env::args().next().unwrap());
 
         std::process::exit(0);
     }

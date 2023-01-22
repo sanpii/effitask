0aa89b462f5c559c6c79380e91ba4307049af621
Jeremy Farmer <jeremy.farmer1206@proton.me>
2023-01-22 15:44:25 -0500

    Moved initialization of done and todo .txt files to EffiEnvironment

--- src/application/environment.rs
+++ src/application/environment.rs
@@ -6,6 +6,20 @@ pub struct EffiEnvironment {
     pub done_file_path: String
 }
 
+fn initialize_effi_file(file_path: &str){
+    // If a todo.txt or done.txt file does not exist, create an empty one
+    if !std::fs::metadata(file_path).is_ok(){
+        log::info!("todo.txt or done.txt does not exist, creating an empty file at {}", file_path);
+        match std::fs::File::create(file_path){
+            Ok(_) => {},
+            Err(err) => {
+                log::error!("Unable to create {} because {}",file_path, err);
+            },
+        }
+    }
+
+}
+
 impl EffiEnvironment {
     pub fn new() -> Self {
         let home_dir = dirs::home_dir();
@@ -23,10 +37,15 @@ impl EffiEnvironment {
         let todo_dir = format!("{}{}", home_dir_str, "/.local/opt/share/todo");
         let todo_file = format!("{}/{}", todo_dir, "todo.txt");
         let done_file = format!("{}/{}", todo_dir, "done.txt");
+
+        initialize_effi_file(&todo_file);
+        initialize_effi_file(&done_file);
+
         Self{
             todo_directory_path: todo_dir,
             todo_file_path: todo_file,
             done_file_path: done_file
         }
     }
+
 }

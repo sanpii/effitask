d1dee058e095c9c12549e6def748bd15eea356c5
Sanpi <sanpi@homecomputing.fr>
2024-04-03 11:59:14 +0200

    Updates pulldown_cmark to 0.10

--- src/tasks/markup.rs
+++ src/tasks/markup.rs
@@ -20,33 +20,35 @@ impl Markup for todo_txt::task::Note {
             use std::fmt::Write;
 
             use pulldown_cmark::Event;
-            use pulldown_cmark::Tag;
+            use pulldown_cmark::{Tag, TagEnd};
 
             match event {
-                Event::Start(Tag::Heading(level, ..)) => {
+                Event::Start(Tag::Heading { level, .. }) => {
                     write!(markup, "<span font_size='{}'><u>", headers[level as usize]).ok();
                 }
-                Event::End(Tag::Heading(..)) => markup.push_str("</u></span>\n\n"),
+                Event::End(TagEnd::Heading(..)) => markup.push_str("</u></span>\n\n"),
 
                 Event::Start(Tag::Paragraph) => markup.push_str("<span>"),
-                Event::End(Tag::Paragraph) => markup.push_str("</span>\n"),
+                Event::End(TagEnd::Paragraph) => markup.push_str("</span>\n"),
 
                 Event::Start(Tag::CodeBlock(_)) => markup.push_str("<tt>"),
-                Event::End(Tag::CodeBlock(_)) => markup.push_str("</tt>"),
+                Event::End(TagEnd::CodeBlock) => markup.push_str("</tt>"),
 
                 Event::Start(Tag::Emphasis) => markup.push_str("<i>"),
-                Event::End(Tag::Emphasis) => markup.push_str("</i>"),
+                Event::End(TagEnd::Emphasis) => markup.push_str("</i>"),
 
                 Event::Start(Tag::Strong) => markup.push_str("<b>"),
-                Event::End(Tag::Strong) => markup.push_str("</b>"),
+                Event::End(TagEnd::Strong) => markup.push_str("</b>"),
 
                 Event::Start(Tag::Item) => markup.push_str("· "),
-                Event::End(Tag::Item) | Event::SoftBreak => markup.push('\n'),
+                Event::End(TagEnd::Item) | Event::SoftBreak => markup.push('\n'),
 
-                Event::Start(Tag::Link(_, link, title)) => {
-                    write!(markup, "<a href='{link}' title='{title}'>").ok();
+                Event::Start(Tag::Link {
+                    dest_url, title, ..
+                }) => {
+                    write!(markup, "<a href='{dest_url}' title='{title}'>").ok();
                 }
-                Event::End(Tag::Link(_, _, _)) => markup.push_str("</a>"),
+                Event::End(TagEnd::Link) => markup.push_str("</a>"),
 
                 Event::Text(t) => markup.push_str(&t.replace('&', "&amp;")),
 
